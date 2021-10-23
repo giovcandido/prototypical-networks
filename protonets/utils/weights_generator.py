2dab63740483a4d0823e42bf26859deadb46c80a
giovcandido <giovcandido@outlook.com>
2021-10-23 16:08:13 -0300

    Update how random weights are generated

--- protonets/utils/weights_generator.py
+++ protonets/utils/weights_generator.py
@@ -1,12 +1,7 @@
 import numpy as np
 
 def generate_random_weights(size):
-    upper_bound = size * 10
-    
-    values_to_pick_from = list(range(1, upper_bound + 1))
-    values_to_pick_from = np.array(values_to_pick_from)
+    values = np.random.uniform(size=size)
+    values_sum = sum(values)
 
-    chosen_values = np.random.choice(values_to_pick_from, size, replace = False)
-    chosen_values_sum = sum(chosen_values)
-
-    return [(x / chosen_values_sum) for x in chosen_values]
+    return np.array([(x / values_sum) for x in values])
